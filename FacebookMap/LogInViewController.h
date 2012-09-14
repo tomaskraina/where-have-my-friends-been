07df58cb28214128eb5d13190573d6c589686569
Tom K <krajeec@gmail.com>
2012-09-14 23:51:59 +0200

    add LogInViewController

---
+++ FacebookMap/LogInViewController.h
@@ -0,0 +1,13 @@
+//
+//  LogInViewController.h
+//  FacebookMap
+//
+//  Created by Tom Kraina on 14.09.2012.
+//  Copyright (c) 2012 tomkraina.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LogInViewController : UIViewController
+
+@end
---
+++ FacebookMap/LogInViewController.m
@@ -0,0 +1,18 @@
+//
+//  LogInViewController.m
+//  FacebookMap
+//
+//  Created by Tom Kraina on 14.09.2012.
+//  Copyright (c) 2012 tomkraina.com. All rights reserved.
+//
+
+#import "LogInViewController.h"
+
+@implementation LogInViewController
+
+- (IBAction)logIn
+{
+    
+}
+
+@end
